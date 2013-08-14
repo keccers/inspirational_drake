a185a7ce2edf57ae5d641665668df73e122cc8fd
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-14 10:03:14 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InspirationalDrake::Application.config.secret_key_base = '847eb732a49558d5befd596a436ba39f7d00ac97a441603bae361fde5d1056a0af93d433eeeffe781425c85744e2b5c1fec7eabeb473f08e74234b15618813f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InspirationalDrake::Application.config.session_store :cookie_store, key: '_inspirational_drake_session'
